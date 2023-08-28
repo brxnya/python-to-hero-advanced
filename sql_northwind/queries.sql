63c8d9892c03a4824c71265d40994d4379d75764
brxnya <nervice@bk.ru>
2023-08-28 09:26:31 +0300

    sub-queries ANY and ALL quantifiers

--- sql_northwind/queries.sql
+++ sql_northwind/queries.sql
@@ -297,3 +297,37 @@ WHERE NOT EXISTS (SELECT orders.order_id FROM orders
 			 	 JOIN order_details USING(order_id)
 			 	 WHERE order_details.product_id=product_id
 			 	 AND order_date BETWEEN '1995-02-01' AND '1995-02-15');
+
+
+-- ↓ sub-queries ANY and ALL quantifiers ↓ --
+
+SELECT DISTINCT company_name
+FROM customers
+JOIN orders USING(customer_id)
+JOIN order_details USING(order_id)
+WHERE quantity > 40;
+-- ↓ --
+SELECT DISTINCT company_name
+FROM customers
+WHERE customer_id = ANY(
+	SELECT customer_id
+	FROM orders
+	JOIN order_details USING(order_id)
+	WHERE quantity > 40);
+
+SELECT DISTINCT product_name, quantity
+FROM products
+JOIN order_details USING(product_id)
+WHERE quantity > (
+	SELECT AVG(quantity)
+	FROM order_details)
+ORDER BY quantity;
+
+SELECT DISTINCT product_name, quantity
+FROM products
+JOIN order_details USING(product_id)
+WHERE quantity > ALL(
+	SELECT AVG(quantity)
+	FROM order_details
+	GROUP BY product_id)
+ORDER BY quantity;
