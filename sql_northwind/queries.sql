5794df853f6067893e18feac041fc123ef29c2b4
brxnya <nervice@bk.ru>
2023-08-28 10:01:27 +0300

    DDL part_1

--- sql_northwind/queries.sql
+++ sql_northwind/queries.sql
@@ -331,3 +331,69 @@ WHERE quantity > ALL(
 	FROM order_details
 	GROUP BY product_id)
 ORDER BY quantity;
+
+
+-- ↓ DDL part_1 ↓ --
+
+CREATE TABLE student
+(
+	student_id serial,
+	first_name varchar,
+	last_name varchar,
+	birthday date,
+	phone varchar
+);
+
+CREATE TABLE cathedra
+(
+	cathedra_id serial,
+	cathedra_name varchar,
+	dean varchar
+);
+
+ALTER TABLE student
+ADD COLUMN middle_name varchar;
+
+ALTER TABLE student
+ADD COLUMN rating float;
+
+ALTER TABLE student
+ADD COLUMN enrolled date;
+
+ALTER TABLE student
+DROP COLUMN middle_name;
+
+ALTER TABLE cathedra
+RENAME TO chair;
+
+ALTER TABLE chair
+RENAME cathedra_id TO chai_id;
+
+ALTER TABLE chair
+RENAME cathedra_name TO chai_name;
+
+ALTER TABLE student
+ALTER COLUMN first_name SET DATA TYPE varchar(64);
+
+ALTER TABLE student
+ALTER COLUMN last_name SET DATA TYPE varchar(64);
+
+ALTER TABLE student
+ALTER COLUMN phone SET DATA TYPE varchar(64);
+
+CREATE TABLE faculty
+(
+	faculty_id serial,
+	faculty_name varchar
+);
+INSERT INTO faculty(faculty_name)
+VALUES
+('faculty1'),
+('faculty2'),
+('faculty3');
+
+SELECT * FROM faculty;
+
+TRUNCATE TABLE faculty RESTART IDENTITY;
+
+DROP TABLE faculty;
