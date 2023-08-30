1c57132359674a761daf3cfe68a374476e3c5277
brxnya <nervice@bk.ru>
2023-08-30 15:38:41 +0300

    DDL - update, delete, returning

--- sql_northwind/queries.sql
+++ sql_northwind/queries.sql
@@ -668,3 +668,47 @@ INSERT INTO best_authors
 SELECT *
 FROM author
 WHERE rating < 4.5;
+
+
+-- ↓ DDL - update, delete, returning ↓ --
+
+SELECT * FROM author;
+
+UPDATE author
+SET full_name = 'Elias', rating = 5
+WHERE author_id = 1;
+
+DELETE FROM author
+WHERE rating < 4.5;
+
+DELETE FROM author;
+
+TRUNCATE TABLE author;
+
+DROP TABLE book;
+
+CREATE TABLE book
+(
+	book_id serial,
+	title text NOT NULL,
+	isbn varchar(32) NOT NULL,
+	publisher_id int NOT NULL,
+
+	CONSTRAINT PK_book_book_id PRIMARY KEY(book_id)
+);
+
+INSERT INTO book(title, isbn, publisher_id)
+VALUES ('title', 'isbn', 3)
+RETURNING *;
+
+INSERT INTO author
+VALUES(1, 'full_name', 4);
+
+UPDATE author
+SET full_name = 'Walter', rating = 5
+WHERE author_id = 1
+RETURNING author_id;
+
+DELETE FROM author
+WHERE rating = 5
+RETURNING *;
