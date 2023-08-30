b34036f8e39d6870cd31998738301a0f60578b2a
brxnya <nervice@bk.ru>
2023-08-30 15:23:13 +0300

    DDL - insert

--- sql_northwind/queries.sql
+++ sql_northwind/queries.sql
@@ -639,3 +639,32 @@ CREATE TABLE book
 INSERT INTO book
 OVERRIDING SYSTEM VALUE
 VALUES(3, 'title', 'isbn', 1);
+
+
+-- ↓ DDL - insert ↓ --
+
+INSERT INTO author
+VALUES(10, 'John Silver', 4.5);
+
+SELECT * FROM author;
+
+INSERT INTO author (author_id, full_name)
+VALUES (11, 'Bob Gray');
+
+INSERT INTO author (author_id, full_name)
+VALUES
+(12, 'Name 1'),
+(13, 'Name 2'),
+(14, 'Name 3');
+
+SELECT *
+INTO best_authors
+FROM author
+WHERE rating >= 4.5;
+
+SELECT * FROM best_authors;
+
+INSERT INTO best_authors
+SELECT *
+FROM author
+WHERE rating < 4.5;
