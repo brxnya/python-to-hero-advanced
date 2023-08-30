9f052868629a1a23ecdb91ce69eb60a120e43b22
brxnya <nervice@bk.ru>
2023-08-30 14:05:31 +0300

    DDL - check

--- sql_northwind/queries.sql
+++ sql_northwind/queries.sql
@@ -505,3 +505,29 @@ CREATE TABLE book
 
 ALTER TABLE book
 DROP CONSTRAINT PK_book_publisher;
+
+
+-- ↓ DDL - check ↓ --
+
+CREATE TABLE book
+(
+	book_id int,
+	title text NOT NULL,
+	isbn varchar(32) NOT NULL,
+	publisher_id int,
+
+	CONSTRAINT PK_book_book_id PRIMARY KEY(book_id)
+);
+
+ALTER TABLE book
+ADD COLUMN price decimal CONSTRAINT CHK_book_price CHECK(price >= 0);
+
+INSERT INTO book
+VALUES
+(1, 'title', 'isbn', 1, 1.5);
+
+SELECT * FROM book;
+
+INSERT INTO book
+VALUES
+(2, 'title', 'isbn', 1, -1.5);
