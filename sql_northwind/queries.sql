b862a7783212ba258bff84e20cbea89de1b2f9de
brxnya <nervice@bk.ru>
2023-08-30 15:02:02 +0300

    DDL - sequences & tables

--- sql_northwind/queries.sql
+++ sql_northwind/queries.sql
@@ -591,3 +591,51 @@ ALTER SEQUENCE seq4 RESTART WITH 16;
 SELECT nextval('seq4');
 
 DROP SEQUENCE seq4;
+
+
+-- ↓ DDL - sequences & tables ↓ --
+
+CREATE TABLE book
+(
+	book_id serial NOT NULL,
+	title text NOT NULL,
+	isbn varchar(32) NOT NULL,
+	publisher_id int NOT NULL,
+
+	CONSTRAINT PK_book_book_id PRIMARY KEY(book_id)
+);
+-- ↓ --
+CREATE TABLE book
+(
+	book_id int NOT NULL,
+	title text NOT NULL,
+	isbn varchar(32) NOT NULL,
+	publisher_id int NOT NULL,
+
+	CONSTRAINT PK_book_book_id PRIMARY KEY(book_id)
+);
+
+CREATE SEQUENCE IF NOT EXISTS book_book_id_seq
+START WITH 1 OWNED BY book.book_id;
+
+ALTER TABLE book
+ALTER COLUMN book_id SET DEFAULT nextval('book_book_id_seq');
+
+INSERT INTO book(title, isbn, publisher_id)
+VALUES('title', 'isbn', 1);
+
+SELECT * FROM book;
+
+CREATE TABLE book
+(
+	book_id int GENERATED ALWAYS AS IDENTITY NOT NOT NULL,
+	title text NOT NULL,
+	isbn varchar(32) NOT NULL,
+	publisher_id int NOT NULL,
+
+	CONSTRAINT PK_book_book_id PRIMARY KEY(book_id)
+);
+
+INSERT INTO book
+OVERRIDING SYSTEM VALUE
+VALUES(3, 'title', 'isbn', 1);
